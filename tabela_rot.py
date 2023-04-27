4ea34e95d04a32dd478a43d47aca494e6544cdb7
Pâmela <72814384+painttw07@users.noreply.github.com>
2023-04-27 20:50:15 -0300

    Add files via upload

--- tabela_rot.py
+++ tabela_rot.py
@@ -3,9 +3,9 @@ import socket
 class Tabela_Rot:
     def __init__(self):
         self.route_table = {
-    '192.168.0.1/2403': '192.168.1.1',  # Próximo salto para a sub-rede 192.168.1.0/24 é o endereço 192.168.1.1
-    '192.168.0.2/2404': '192.168.2.1',  # Próximo salto para a sub-rede 192.168.2.0/24 é o endereço 192.168.2.1
-    '192.168.0.3/2405': '192.168.3.1'  # Próximo salto para a sub-rede 10.0.0.0/8 é o endereço 10.0.0.1
+    '192.168.0.1/2403': '192.168.1.1',  # Próximo salto para a sub-rede 192.168.0.1/2403 é o endereço 192.168.1.1
+    '192.168.0.2/2404': '192.168.2.1',  # Próximo salto para a sub-rede 192.168.0.2/2404 é o endereço 192.168.2.1
+    '192.168.0.3/2405': '192.168.3.1'  # Próximo salto para a sub-rede 192.168.0.3/2405 é o endereço 192.168.3.1
 }
 #(i) rede de destino, (ii) interface de saída, (iii) métrica
 # Exibição da tabela de roteamento
