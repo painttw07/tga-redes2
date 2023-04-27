4e4830260f13371f7597088ab4b13bf3d0d15c59
Pâmela <72814384+painttw07@users.noreply.github.com>
2023-04-27 20:54:37 -0300

    Add files via upload

--- dados.py
+++ dados.py
@@ -1,14 +1,14 @@
 import socket
 
 #(i) endereço IP de origem, (ii) endereço IP de destino, (iii) TTL e(iv) TOS.
-    
+#classe de dados - o que deveria conter no pacote de dados
 class Dados:
     def _init_(self, orig, dest, TTL, TOS):
-        self.host
-        self.port
-        self.orig = ori
-        self.dest = dest
-        self.TTL = TTL
-        selft.TOS = TOS
+        self.host #host
+        self.port #porta
+        self.orig = ori #endereço origem
+        self.dest = dest #endereço destino
+        self.TTL = TTL #ttl
+        selft.TOS = TOS #tos
 
 
